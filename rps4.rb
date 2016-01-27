c94cbe833eaa446cdeee21775bd1830975feb328
DMelon113 <dmelonid@lsoc.org>
2016-01-27 13:55:19 -0600

    DONE

--- rps4.rb
+++ rps4.rb
@@ -3,11 +3,11 @@ win = 0
 #set loses variable to zero
 losses = 0
 #Prompt user to chose r, p or s
-puts "Choose r, p, or s"
-#get user input, set to choice variable, make sure it's lowercase
-choice = gets.chomp.downcase
+history = []#get user input, set to choice variable, make sure it's lowercase
 #Select computer choice randomly from arrary, set to variable computer
 3.times do
+puts "Choose r, p, or s"
+choice = gets.chomp.downcase
   computer = ['r', 'p', 's'].sample
 
 #Three time do iterate
@@ -24,7 +24,7 @@ choice = gets.chomp.downcase
       puts "You lose"
       losses = losses + 1
 end
-
+history << [choice, computer]
 end
 
 puts win
@@ -40,4 +40,5 @@ elsif losses > win
 
 #### BONUS ####
   #Show history of computer and user moves at end.
-
+puts ''
+print history
